f619931c6fb8c47413db358fc10eec43f5edf377
Dewey Dunnington <dewey@wherobots.com>
2025-09-04 20:06:00 -0500

    fix setsrid

--- rust/sedona-functions/src/st_setsrid.rs
+++ rust/sedona-functions/src/st_setsrid.rs
@@ -214,13 +214,8 @@ mod test {
         assert_value_equal(&result, &geom_lnglat);
 
         // Call with an integer code of 0 (should unset the output crs)
-        let (return_type, result) = call_udf(
-            &udf,
-            geom_lnglat.clone(),
-            WKB_GEOMETRY,
-            unset_scalar.clone(),
-        )
-        .unwrap();
+        let (return_type, result) =
+            call_udf(&udf, geom_lnglat.clone(), wkb_lnglat, unset_scalar.clone()).unwrap();
         assert_eq!(return_type, WKB_GEOMETRY);
         assert_value_equal(&result, &geom_arg);
 
