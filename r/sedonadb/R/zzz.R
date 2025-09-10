f45deccac02abf1dec3665af5691901235b924f4
Dewey Dunnington <dewey@wherobots.com>
2025-09-10 15:01:18 -0500

    fix readme

--- r/sedonadb/R/zzz.R
+++ r/sedonadb/R/zzz.R
@@ -19,8 +19,7 @@
   # Load geoarrow to manage conversion of arrow results to/from spatial objects
   requireNamespace("geoarrow", quietly = TRUE)
 
-  # Call at least one function for R CMD check
-  geoarrow::as_geoarrow_array("POINT (0 1")
+  s3_register("sf::st_as_sf", "sedonadb_dataframe")
 
   # Inject what we need to reduce the Rust code to a simple Rf_eval()
   ns <- asNamespace("sedonadb")
@@ -35,4 +34,82 @@ check_interrupts <- function() {
   tryCatch({
     FALSE
   }, interrupt = function(...) TRUE, error = function(...) TRUE)
+}
+
+# Permissively licensed (unlicense) from
+# https://github.com/r-lib/vctrs/blob/b63a233bd8b8d6511cf3f7d9182c359a3b8c3cab/R/register-s3.R
+s3_register <- function(generic, class, method = NULL) {
+  stopifnot(is.character(generic), length(generic) == 1)
+  stopifnot(is.character(class), length(class) == 1)
+
+  pieces <- strsplit(generic, "::")[[1]]
+  stopifnot(length(pieces) == 2)
+  package <- pieces[[1]]
+  generic <- pieces[[2]]
+
+  caller <- parent.frame()
+
+  get_method_env <- function() {
+    top <- topenv(caller)
+    if (isNamespace(top)) {
+      asNamespace(environmentName(top))
+    } else {
+      caller
+    }
+  }
+  get_method <- function(method) {
+    if (is.null(method)) {
+      get(paste0(generic, ".", class), envir = get_method_env())
+    } else {
+      method
+    }
+  }
+
+  register <- function(...) {
+    envir <- asNamespace(package)
+
+    # Refresh the method each time, it might have been updated by
+    # `devtools::load_all()`
+    method_fn <- get_method(method)
+    stopifnot(is.function(method_fn))
+
+
+    # Only register if generic can be accessed
+    if (exists(generic, envir)) {
+      registerS3method(generic, class, method_fn, envir = envir)
+    } else if (identical(Sys.getenv("NOT_CRAN"), "true")) {
+      warn <- .rlang_s3_register_compat("warn")
+
+      warn(c(
+        sprintf(
+          "Can't find generic `%s` in package %s to register S3 method.",
+          generic,
+          package
+        ),
+        "i" = "This message is only shown to developers using devtools.",
+        "i" = sprintf("Do you need to update %s to the latest version?", package)
+      ))
+    }
+  }
+
+  # Always register hook in case package is later unloaded & reloaded
+  setHook(packageEvent(package, "onLoad"), function(...) {
+    register()
+  })
+
+  # For compatibility with R < 4.0 where base isn't locked
+  is_sealed <- function(pkg) {
+    identical(pkg, "base") || environmentIsLocked(asNamespace(pkg))
+  }
+
+  # Avoid registration failures during loading (pkgload or regular).
+  # Check that environment is locked because the registering package
+  # might be a dependency of the package that exports the generic. In
+  # that case, the exports (and the generic) might not be populated
+  # yet (#1225).
+  if (isNamespaceLoaded(package) && is_sealed(package)) {
+    register()
+  }
+
+  invisible()
 }
