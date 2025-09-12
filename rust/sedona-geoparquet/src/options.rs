a06eb8106cd094587dc6f68fd885d6099c390a18
Dewey Dunnington <dewey@wherobots.com>
2025-09-12 15:08:00 -0500

    document options

--- rust/sedona-geoparquet/src/options.rs
+++ rust/sedona-geoparquet/src/options.rs
@@ -3,7 +3,9 @@ use datafusion::config::TableParquetOptions;
 /// [TableParquetOptions] wrapper with GeoParquet-specific options
 #[derive(Debug, Default, Clone)]
 pub struct TableGeoParquetOptions {
+    /// Inner [TableParquetOptions]
     pub inner: TableParquetOptions,
+    /// [GeoParquetVersion] to use when writing GeoParquet files
     pub geoparquet_version: GeoParquetVersion,
 }
 
@@ -16,12 +18,39 @@ impl From<TableParquetOptions> for TableGeoParquetOptions {
     }
 }
 
+/// The GeoParquet Version to use
 #[derive(Debug, Clone, Copy)]
 pub enum GeoParquetVersion {
+    /// Automatically determine which GeoParquet version to use on write
+    ///
+    /// The heurisitc is currently to include GeoParquet 1.0 if a table contains
+    /// a spatial type, or Omitted otherwise.
     Auto,
+
+    /// Write GeoParquet 1.0 metadata
     V1_0,
+
+    /// Write GeoParquet 1.1 metadata and optional bounding box column
+    ///
+    /// A bbox column will be included for any column where the Parquet options would
+    /// have otherwise written statistics (which it will by default).
+    /// This option may be more computationally expensive; however, will result in
+    /// row-group level statistics that some readers (e.g., SedonaDB) can use to prune
+    /// row groups on read.
     V1_1,
+
+    /// Write GeoParquet 2.0
+    ///
+    /// The GeoParquet 2.0 options is identical to GeoParquet 1.0 except the underlying storage
+    /// of spatial columns is Parquet native geometry, where the Parquet writer will include
+    /// native statistics according to the underlying Parquet options. Some readers
+    /// (e.g., SedonaDB) can use these statistics to prune row groups on read.
     V2_0,
+
+    /// Do not write GeoParquet metadata
+    ///
+    /// This option suppresses GeoParquet metadata; however, spatial types will be written as
+    /// Parquet native Geometry/Geography when this is supported by the underlying writer.
     Omitted,
 }
 
